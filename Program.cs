de735a78ca0a524bb5c7fcc522c84f8c083517c8
Samuel Caldas <samuel.caldas@gmail.com>
2020-04-24 19:28:04 -0300

    Atualizando nomes de variáveis para melhorar a legibilidade.

--- Program.cs
+++ Program.cs
@@ -27,67 +27,68 @@ namespace ppo.net
                     dynamic np = Py.Import("numpy");    // Numpy para trabalhar com arrays
                     dynamic PPO = Py.Import("PPO");
                     dynamic plt = Py.Import("matplotlib.pyplot");
+
                     //  Configurações   //
-                    int EP_MAX = 700;   // Quantidade total de episódios
-                    int EP_LEN = 200;   // Quantas sequencias vão acontecer dentro de cada episódio
+                    int TRAINING_EPOCHS = 700;   // Quantidade total de episódios
+                    int TRAINING_STEPS = 200;   // Quantas sequencias vão acontecer dentro de cada episódio
                     double GAMMA = 0.9; // Avanço (?)
-                    int BATCH = 64;     // Tamanho do pacote à entrar para treinamento em cada etapa (?)
+                    int NUMBER_OF_SAMPLES = 64;     // Tamanho do pacote à entrar para treinamento em cada etapa (?)
 
                     // Implementação do ambiente   //
                     dynamic env = gym.make("Pendulum-v0").unwrapped;    // Instancia o ambiente pendulo
-                    dynamic ppo = PPO.PPO();                            // Instancia a classe PPO
-                    List<double> all_ep_r = new List<double>();         // Cria um array para a recompensa de todos os episódios
+                    dynamic ppo = PPO.PPO();                            // Instancia action classe PPO
+                    List<double> all_epochs_rewards = new List<double>();         // Cria um array para action recompensa de todos os episódios
 
                     //  Loop de episódios //
-                    for (int epmax = 0; epmax < EP_MAX; epmax++)    // EP_MAX: quantidade de episódios 
+                    for (int EPOCH = 0; EPOCH < TRAINING_EPOCHS; EPOCH++)    // TRAINING_EPOCHS: quantidade de episódios 
                     {
-                        dynamic s = env.reset();     // Redefine o ambiente e armazena o estado atual em s
+                        dynamic state = env.reset();     // Redefine o ambiente e armazena o estado atual em state
                         // Cria quatro arrais para o episódio:
-                        List<PyObject> buffer_s = new List<PyObject>(); // buffer_s: buffer do estado
-                        List<PyObject> buffer_a = new List<PyObject>(); // buffer_a: buffer da ação
-                        List<double> buffer_r = new List<double>();     // buffer_r: buffer da recompensa         
-                        List<double> discounted_r = new List<double>(); // Cria um array pra armazenar as recompensas calculadas
-                        double ep_r = 0;    // Recompensa do episódio
+                        List<PyObject> buffered_states = new List<PyObject>(); // buffered_states: buffer do estado
+                        List<PyObject> buffered_actions = new List<PyObject>(); // buffered_actions: buffer da ação
+                        List<double> buffered_reward = new List<double>();     // buffered_reward: buffer da recompensa         
+                        List<double> discounted_reward = new List<double>(); // Cria um array pra armazenar as recompensas calculadas
+                        double epoch_reward = 0;    // Recompensa do episódio
                         //  Loop de episódio //
-                        for (int eplen = 0; eplen < EP_LEN; eplen++)    // Duração de cada episodio
+                        for (int STEP = 0; STEP < TRAINING_STEPS; STEP++)    // Duração de cada episodio
                         {
                             env.render();   // Renderiza o ambiente
-                            var a = ppo.choose_action(s);   // Envia um estado s e recebe uma ação a 
-                            dynamic step = env.step(a);     // Envia uma ação a ao ambiente e recebe o estado s_, e a recompensa r
-                            PyObject s_ = step.GetItem(0);  // Adiciona ao buffer de estado o estado atual s
-                            dynamic r = step.GetItem(1);
-                            buffer_s.Add(s);    // Adiciona ao buffer de estado o estado atual s
-                            buffer_a.Add(a);    // Adiciona ao buffer de ação a ação atual a
-                            double _r = r;
-                            buffer_r.Add((_r + 8) / 8); // Adiciona ao buffer de recompensa a recompensa atual (?) normalizada (r+8)/8
-                            s = s_;     // Atualiza a variável de estado com o estado recebido pelo ambiente
-                            ep_r += _r; // soma a recompensa da ação a recompensa do episodio
+                            var action = ppo.choose_action(state);   // Envia um estado state e recebe uma ação action 
+                            dynamic step = env.step(action);     // Envia uma ação action ao ambiente e recebe o estado step_state, e action recompensa reward
+                            PyObject step_state = step.GetItem(0);  // Adiciona ao buffer de estado o estado atual state
+                            dynamic reward = step.GetItem(1);
+                            buffered_states.Add(state);    // Adiciona ao buffer de estado o estado atual state
+                            buffered_actions.Add(action);    // Adiciona ao buffer de ação action ação atual action
+                            double _reward = reward;
+                            buffered_reward.Add((_reward + 8) / 8); // Adiciona ao buffer de recompensa action recompensa atual (?) normalizada (reward+8)/8
+                            state = step_state;     // Atualiza action variável de estado com o estado recebido pelo ambiente
+                            epoch_reward += _reward; // soma action recompensa da ação action recompensa do episodio
 
                             //  Atualiza PPO //
-                            if ((eplen + 1) % BATCH == 0 || eplen == EP_LEN - 1)
+                            if ((STEP + 1) % NUMBER_OF_SAMPLES == 0 || STEP == TRAINING_STEPS - 1)
                             {
-                                double v_s_ = ppo.get_v(s_);    // Passa o estado atual s_ e recebe o valor atual da taxa de aprendizagem da CRITICA
+                                double v_s_ = ppo.get_v(step_state);    // Passa o estado atual step_state e recebe o valor atual da taxa de aprendizagem da CRITICA
                                                                 // V = learned state-value function
-                                discounted_r.Clear();           // Limpa o array de recompensas calculadas
-                                buffer_r.Reverse();             // Coloca o array de buffer de recompensa ao contrario
-                                foreach (double b_r in buffer_r)
+                                discounted_reward.Clear();           // Limpa o array de recompensas calculadas
+                                buffered_reward.Reverse();             // Coloca o array de buffer de recompensa ao contrario
+                                foreach (double b_r in buffered_reward)
                                 {
-                                    v_s_ = b_r + GAMMA * v_s_;  // Calcula a recompensa multiplicando a recompensa recebida r pela GAMMA 
+                                    v_s_ = b_r + GAMMA * v_s_;  // Calcula action recompensa multiplicando action recompensa recebida reward pela GAMMA 
                                                                 // e pelo valor da taxa de aprendizado do estado v_s_
-                                    discounted_r.Add(v_s_);     // Adiciona ao array de recompensas calculadas 
+                                    discounted_reward.Add(v_s_);     // Adiciona ao array de recompensas calculadas 
                                 }
-                                discounted_r.Reverse();         // Coloca o array de recompensas calculadas ao contrario
+                                discounted_reward.Reverse();         // Coloca o array de recompensas calculadas ao contrario
                                 // vstack transforma os arrays que estão em linha, em colunas
                                 // Esses arrays de colunas sao armazenados em bs ba e br
-                                var bs = np.vstack(buffer_s);
-                                var ba = np.vstack(buffer_a);
-                                var _br = np.array(discounted_r);
+                                var bs = np.vstack(buffered_states);
+                                var ba = np.vstack(buffered_actions);
+                                var _br = np.array(discounted_reward);
                                 var br = np.expand_dims(_br, axis: 1);
                                 // Esvazia os buffers de estado, ação e recompensa
-                                buffer_s.Clear();
-                                buffer_a.Clear();
-                                buffer_r.Clear();
-                                // Treine o cliente e o ator (status, ações, desconto de r)
+                                buffered_states.Clear();
+                                buffered_actions.Clear();
+                                buffered_reward.Clear();
+                                // Treine o cliente e o ator (status, ações, desconto de reward)
                                 ppo.update( // Atualiza as redes com:
                                     bs,     //   Os estados acumulados
                                     ba,     //   As ações acumuladas
@@ -95,26 +96,26 @@ namespace ppo.net
                                 );
                             }
                         }
-                        // Adiciona a recompensa do episodio atual ao array de recompensas
-                        if (epmax == 0)
+                        // Adiciona action recompensa do episodio atual ao array de recompensas
+                        if (EPOCH == 0)
                         {
-                            all_ep_r.Add(ep_r);
+                            all_epochs_rewards.Add(epoch_reward);
                         }
                         else
                         {
-                            all_ep_r.Add(all_ep_r.Last() * GAMMA + ep_r * 0.1);
+                            all_epochs_rewards.Add(all_epochs_rewards.Last() * GAMMA + epoch_reward * 0.1);
                         }
                         // Escreve na tela
                         Console.WriteLine(
-                            "Ep:    " + epmax +      // Numero do episodio
-                            "   |   Ep_r:   " + ep_r // Recompensa do episodio
+                            "Ep:    " + EPOCH +      // Numero do episodio
+                            "   |   epoch_reward:   " + epoch_reward // Recompensa do episodio
                         );
                     }
                     plt.plot( // Plota o gráfico de todas as recompensas
                         np.arange(
-                            all_ep_r.Count
+                            all_epochs_rewards.Count
                         ),
-                        all_ep_r
+                        all_epochs_rewards
                     );
                     plt.xlabel("Episode");
                     plt.ylabel("Moving averaged episode reward");
